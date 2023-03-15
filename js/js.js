97155e0f44fcfb56d84c11c661b4925b4403e197
Ibrahim Aboubacar Cisse <ibrahimaboubacarcisee@gmail.com>
2023-03-15 22:41:47 +0000

    implementing the logo of the site and the page title on mobile view

--- js/js.js
+++ js/js.js
@@ -1,8 +1,33 @@
 const nav = document.querySelector('.nav');
-const navBtn = document.createElement('div');
-navBtn.innerHTML = "<span></span><span></span><span></span>"
-navBtn.classList.add('nav_btn')
-nav.appendChild(navBtn)
-navBtn.onclick = (e) =>{
-    nav.classList.toggle('opened');
+
+
+console.log('1')
+
+// call to functions
+initSiteIcon(nav)
+initPageTitle(nav)
+initTogglerBtn(nav)
+
+// functions
+function initTogglerBtn(nav){
+    const navBtn = document.createElement('div');
+    navBtn.innerHTML = "<span></span><span></span><span></span>"
+    navBtn.classList.add('nav_btn')
+    nav.appendChild(navBtn)
+    navBtn.onclick = (e) =>{
+        nav.classList.toggle('opened');
+    }
+}
+
+function initSiteIcon(nav){
+    const sieIcon = document.createElement('div');
+    sieIcon.innerHTML = '<img src="images/site_icon.png" alt="Site icon">'
+    sieIcon.classList.add('site_icon')
+    nav.appendChild(sieIcon)
+}
+function initPageTitle(nav){
+    const title = document.querySelector('title').innerText
+    const h1 = document.createElement('h1')
+    h1.textContent = title
+    nav.appendChild(h1)
 }
