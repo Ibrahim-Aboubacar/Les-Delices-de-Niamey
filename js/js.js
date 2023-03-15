0cc6fc30556d822141498105d568d96e3eba8e63
Ibrahim Aboubacar Cisse <ibrahimaboubacarcisee@gmail.com>
2023-03-15 22:02:49 +0000

    implementing the van tooglers code in js

--- js/js.js
+++ js/js.js
@@ -3,3 +3,6 @@ const navBtn = document.createElement('div');
 navBtn.innerHTML = "<span></span><span></span><span></span>"
 navBtn.classList.add('nav_btn')
 nav.appendChild(navBtn)
+navBtn.onclick = (e) =>{
+    nav.classList.toggle('opened');
+}
